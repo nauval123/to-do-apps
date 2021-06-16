c52ba6bbfdfb28aac790f3255c3cb4548a3ea6a9
Nauval Achmad Yusufa <nayuya24@gmail.com>
2021-06-16 21:28:23 +0700

    fetching data (Read Data)

---
+++ lib/main.dart
@@ -0,0 +1,60 @@
+// import 'dart:html';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({ Key key , this.title }) : super(key: key);
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: FutureBuilder(
+        future: Firestore.instance.collection('todo').getDocuments(), 
+        builder: (context, snapshot) {
+          if (!snapshot.hasData) {
+            return Center(
+              child: CircularProgressIndicator(
+                backgroundColor: Colors.blue,
+              ),
+            );
+          }
+           return ListView.builder(
+            itemCount: snapshot.data.documents.length,
+            itemBuilder: (context, index) {
+              return ListTile(
+                  title: Text(snapshot.data.documents[index]['task']),
+                subtitle: Text(snapshot.data.documents[index]['description']),
+              );
+            },
+          );
+        },),
+    );
+  }
+}
